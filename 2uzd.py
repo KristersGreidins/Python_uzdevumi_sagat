a2b45e9702da31a82222a5c4faa0a76d2bbd24c4
KristersGreidins <greidins11@gmail.com>
2023-11-03 14:47:45 +0200

    3.uzdevums izlabots

--- 2uzd.py
+++ 2uzd.py
@@ -1,4 +1,10 @@
 """ 
 Iegūt ziņas, izmantojot RSS plūsmu no google.lv.
 
+Kas ir RSS?
+1) pip install feedparser
+2) Ja nepieciešams pip upgrade veicam!
+
 """
+
+
--- 3uzd.py
+++ 3uzd.py
@@ -5,3 +5,11 @@ piemēram, vārdu, vecumu un ceļojumam iemaksāto summu.
 
 """
 
+class Dati:
+    def __init__(self,vards,vecums,iem_summa):
+        self.vards=vards
+        self.vecums=vecums
+        self.iem_summa=iem_summa
+
+kkk=Dati("Pēteris", 20, 0.5)
+        
